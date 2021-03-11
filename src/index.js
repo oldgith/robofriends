afcfe037a89767d27ec687526c3c66bc0cae5dd1
iamfarraz <farazanwar8@gmail.com>
2021-03-11 20:16:43 +0530

    Card lis ready

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {robots} from './robots'
+import CardList from './CardList'
+import 'tachyons'; 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   
+    <div>
+<CardList robots={robots} />   
+    </div>
+,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA -vitals
 reportWebVitals();
