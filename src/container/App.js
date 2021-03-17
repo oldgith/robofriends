4407e27930e6691dcdbdc1b9cd552cdc2a4f1c6a
iamfarraz <farazanwar8@gmail.com>
2021-03-18 02:38:03 +0530

    hooks implemnted

--- src/container/App.js
+++ src/container/App.js
@@ -1,56 +1,45 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import './App.css'
 
-class App extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            'robots':[],
-            searchFeild:''
-        }
-        console.log("constructor")
-    }
-    //not a user defined method so simply use it
-   
-    componentDidMount(){
-   
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res)=>res.json())
-    .then((user)=>this.setState({robots:user}));
-    // const res=await fetch("https://jsonplaceholder.typicode.com/users");
-    // const data= res.json()
-    // this.setState({robots:data})
-    }
 
-    onSearchChange=(event)=>{
-        this.setState({searchFeild:event.target.value})
-     }
+const App=()=>{
+  
+    const [robots,setRobots]=useState([]);
+    const [searchFeild,setSearchFeild]=useState('');
+   
+        useEffect(()=>{
+            fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res)=>res.json())
+            .then((user)=>setRobots(user) )
+            .catch(err=> console.log("error in api",err))  
+            },[])
+       //this second param empty array is used so which tell run this only first time(thts wht componentdidMount was doin)
+       // if we not put empty array it will refetch infintly  many times
 
-    render(){
-        console.log("render");
-        const {robots,searchFeild}=this.state;
-        const filteredRobots=robots.filter((cur)=>{
-       return cur.name.toLowerCase().includes(searchFeild.toLowerCase());
-        }) 
-        if(robots.length===0){
-          return   <h1>Loading</h1>
-        }
-        else {
- return (
-            <div className="tc ">
-         <h1 className="f1">Robo-Friends</h1>
-         <SearchBox searchChange={this.onSearchChange}/>
-         <Scroll>
-         <CardList robots={filteredRobots}/>
-         </Scroll>
-            </div>
-      );
-        }
+   console.log(robots,searchFeild);
+  const  onSearchChange=(event)=>setSearchFeild(event.target.value);
+       
+  
+ const filteredRobots=robots.filter((cur)=>cur.name.toLowerCase().includes(searchFeild.toLowerCase()) ) 
+  
+             
+     if(!robots.length){  return   <h1>Loading</h1> }
+       else {
+            return (
+                    <div className="tc ">
+                    <h1 className="f1">Robo-Friends</h1>
+                    <SearchBox searchChange={onSearchChange}/>
+                    <Scroll>
+                    <CardList robots={filteredRobots}/>
+                    </Scroll>
+                    </div>
+                );
+          }
      
-    }
+    
 }
 
 export default App;
